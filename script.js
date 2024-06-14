897a52d4192aaeafd0acc70f975747d29f390183
”Danny <danny.hearn@outlook.com>
2024-06-14 16:50:54 -0700

    adding the UX to the web page, added a pointer cursor to a class selector

--- script.js
+++ script.js
@@ -18,50 +18,11 @@ function getPlayersChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-function playGame() {
-    for (let round = 1; round <= 5; round++) {
-    let computerChoice = getComputerChoice();
-    let playerChoice = getPlayersChoice();
+const choiceRock = document.querySelector('#rock');
 
-    console.log(`Player's Choice: ${playerChoice} | Computer's Choice: ${computerChoice}`);
-
-    if (computerChoice === playerChoice) {
-        console.log("It's a tie!");
-    } else if (computerChoice === 'rock' && playerChoice === 'paper') {
-        humanScore ++
-        console.log ("Player wins, paper beats rock");
-    } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
-        humanScore ++
-        console.log ("Player wins, rock beats scissors");
-    } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
-        humanScore ++
-        console.log ("Player wins, scissors beats paper");
-    } else {
-        computerScore ++
-        console.log (`Computer wins, ${computerChoice} beats ${playerChoice}`);
-    }
-
-    console.log (`These are the scores: Player: ${humanScore} Computer ${computerScore}`);
-    console.log('------------------------------');
-
-}
+function choice() {
+    alert('You chose Rock!')
 }
 
-function finalResult() {
-    console.log ('------------------------------');
-    if (humanScore === computerScore){
-        console.log('No one wins! Its a tie!');
-    }
-    else if (humanScore > computerScore){
-        console.log(`FINAL SCORES: Player result: ${humanScore} | Computer result: ${computerScore} - Human wins!`);
-    }
-    else if (computerScore > humanScore){
-        console.log(`FINAL SCORES: Player result: ${humanScore} | Computer result: ${computerScore} - Computer wins!`);
-    }
- }
-
- console.log(playGame());
- console.log(finalResult());
-
- hello everyone this is a test
+choiceRock.addEventListener('click', choice);
 
