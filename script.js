21763dfe075d54cbf1575834da3adcbed137299b
”Danny <danny.hearn@outlook.com>
2024-06-14 14:47:50 -0700

    added the player and computer choices at the top of each round

--- script.js
+++ script.js
@@ -23,15 +23,17 @@ function playGame() {
     let computerChoice = getComputerChoice();
     let playerChoice = getPlayersChoice();
 
+    console.log(`Player's Choice: ${playerChoice} | Computer's Choice ${computerChoice}`);
+
     if (computerChoice === playerChoice) {
         console.log("It's a tie!");
-    } else if (computerChoice === 'rock' || playerChoice === 'paper') {
+    } else if (computerChoice === 'rock' && playerChoice === 'paper') {
         humanScore ++
         console.log ("Player wins, paper beats rock");
-    } else if (computerChoice === 'scissors' || playerChoice === 'rock') {
+    } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
         humanScore ++
         console.log ("Player wins, rock beats scissors");
-    } else if (computerChoice === 'paper' || playerChoice === 'scissors') {
+    } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
         humanScore ++
         console.log ("Player wins, scissors beats paper");
     } else {
@@ -39,8 +41,26 @@ function playGame() {
         console.log (`Computer wins, ${computerChoice} beats ${playerChoice}`);
     }
 
-    console.log (`These are the scores for both players: Player: ${humanScore} Computer ${computerScore}`);
+    console.log (`These are the scores: Player: ${humanScore} Computer ${computerScore}`);
+    console.log('------------------------------');
+
 }
 }
 
+function finalResult() {
+    console.log ('------------------------------');
+    if (humanScore === computerScore){
+        console.log('No one wins! Its a tie!');
+    }
+    else if (humanScore > computerScore){
+        console.log(`Player result: ${humanScore} | Computer result: ${computerScore} | Human wins!`);
+    }
+    else if (computerScore > humanScore){
+        console.log(`Player result: ${humanScore} | Computer result: ${computerScore} | Computer wins!`);
+    }
+ }
+
 console.log(playGame());
+console.log(finalResult());
+
+
