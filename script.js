f9aeef82dfaaecf4fdf667b1133656da71555fd3
”Danny <danny.hearn@outlook.com>
2024-06-14 11:14:58 -0700

    addign player choice

--- script.js
+++ script.js
@@ -3,9 +3,17 @@
 
 const options = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice () {
+function getComputerChoice() {
     let move = Math.floor(Math.random() * options.length);
     return options[move];
 }
-
 console.log(getComputerChoice());
+
+//Get player's choice
+
+function getPlayersChoice() {
+    let choice = prompt('Please enter your go:');
+    return choice.toLowerCase();
+}
+
+console.log(getPlayersChoice());
